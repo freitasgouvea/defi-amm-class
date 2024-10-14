a6c8fd03ea35f6ad1a47ef1d0eb9f58e121a0be1
Flavio Gouvea <flaviogouveaneto@gmail.com>
2024-10-14 00:20:28 -0300

    update tests

--- test/AMMPool.t.sol
+++ test/AMMPool.t.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "../src/AMMPool.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { AMMPool } from "../src/AMMPool.sol";
 import { TestERC20 } from "../src/TestERC20.sol";
 
 contract AMMPoolTest is Test {
